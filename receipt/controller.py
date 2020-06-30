00a5e2e146a2a05530c0a44d27f7472ab9c5b47a
nasim seifi <nsm.seifi@gmail.com>
2020-06-30 14:48:13 -0400

    first commit

---
+++ receipt/controller.py
@@ -0,0 +1,140 @@
+import os
+from random import randint
+from controller_model import BasicController
+from helper import Http_error, Http_response
+from log import LogMsg, logger
+from messages import Message
+from .models import Store
+from configs import SIGNUP_USER
+from .constants import STORE_ADD_SCHEMA_PATH
+from .repository import StoreRepository
+
+save_path = os.environ.get('save_path')
+
+store_schemas = dict(add=STORE_ADD_SCHEMA_PATH)
+
+class StoreController(BasicController):
+
+    def __init__(self):
+        super(StoreController, self).__init__(Store, StoreRepository, store_schemas)
+
+    def add(self, db_session, data, username, schema_checked=False,
+            permission_checked=False):
+        logger.info(LogMsg.START, username)
+
+        if username is not None and username == SIGNUP_USER:
+            permission_checked = True
+        name = data.get('name')
+        query_data = dict(filter=dict(name=name))
+        if name:
+            store_by_name = super(StoreController, self).get_by_data(query_data,
+                                                                     db_session)
+            if store_by_name is not None:
+                logger.error(LogMsg.ALREADY_EXISTS, {'name': name})
+                raise Http_error(409, Message.ALREADY_EXISTS)
+
+        email = data.get('email')
+
+        if email:
+            query_data = dict(filter=dict(email=email))
+            store_by_email = super(StoreController, self).get_by_data(query_data,
+                                                                      db_session)
+            if store_by_email is not None:
+                raise Http_error(409, Message.ALREADY_EXISTS)
+
+        data['store_code'] = '{}-{}'.format(name,randint(1000000,9999999))
+        model_instance = super(StoreController, self).add(data, db_session, username,
+                                                           schema_checked,
+                                                           permission_checked)
+        logger.info(LogMsg.END)
+        return model_instance.to_dict()
+
+    def get(self, id, db_session, username=None, permission_checked=False):
+        logger.info(LogMsg.START, username)
+
+        logger.debug(LogMsg.MODEL_GETTING)
+        store = super(StoreController, self).get(id, db_session, username,
+                                                   permission_checked)
+        return store.to_dict()
+
+    def edit(self, id, db_session, data, username, permission_checked=False):
+        logger.info(LogMsg.START, username)
+
+        logger.debug(LogMsg.EDIT_REQUST, {'store_id': id, 'data': data})
+        model_instance = super(StoreController, self).get(id, db_session)
+        if 'store_code' in data:
+            del data['store_code']
+        if 'id' in data:
+            del data['id']
+        if 'name' in data:
+            data['store_code'] = '{}-{}'.format(data['name'], randint(1000000, 9999999))
+        try:
+            super(StoreController, self).edit(id, data, db_session, username,
+                                               permission_checked)
+
+            logger.debug(LogMsg.MODEL_ALTERED, model_instance.to_dict())
+
+            # TODO change def unique
+        except:
+            logger.exception(LogMsg.EDIT_FAILED, exc_info=True)
+            raise Http_error(500, Message.DELETE_FAILED)
+
+        logger.info(LogMsg.END)
+        return model_instance.to_dict()
+
+    def delete(self, id, db_session, username, permission_checked=False):
+        logger.info(LogMsg.START, username)
+
+        logger.info(LogMsg.DELETE_REQUEST, {'store_id': id})
+        try:
+            super(StoreController, self).delete(id, db_session, username,
+                                                 permission_checked)
+
+        except Exception as e:
+            logger.exception(LogMsg.DELETE_FAILED, exc_info=True)
+            raise Http_error(500, Message.DELETE_FAILED)
+
+        logger.info(LogMsg.END)
+
+        return Http_response(204, True)
+
+    def get_all(self, db_session, username, permission_checked=False):
+        logger.info(LogMsg.START, username)
+        try:
+            result = super(StoreController, self).get_all({}, db_session, username,
+                                                           permission_checked)
+            logger.debug(LogMsg.GET_SUCCESS)
+        except:
+            logger.error(LogMsg.GET_FAILED)
+            raise Http_error(400, LogMsg.GET_FAILED)
+        logger.debug(LogMsg.END)
+        return result
+
+    def search_store(self, data, db_session, username, permission_checked=False):
+        logger.info(LogMsg.START, username)
+        logger.debug(LogMsg.GET_ALL_REQUEST, {'username': username, 'data': data})
+        try:
+            stores = super(StoreController, self).get_all(data, db_session, username,
+                                                            permission_checked)
+
+            result = [store.to_dict() for store in stores]
+            logger.debug(LogMsg.GET_SUCCESS, result)
+        except Exception as e:
+            logger.exception(LogMsg.GET_FAILED, exc_info=True)
+            raise Http_error(404, Message.NOT_FOUND)
+        logger.info(LogMsg.END)
+        return result
+
+    def get_store_profile(self, id, db_session, username, permission_checked=False):
+        logger.info(LogMsg.START, username)
+
+        logger.debug(LogMsg.MODEL_GETTING)
+        model_instance = super(StoreController, self).get(id, db_session, username,
+                                                           permission_checked)
+        result = model_instance.to_dict()
+        logger.debug(LogMsg.GET_SUCCESS, result)
+        logger.info(LogMsg.END)
+
+        return result
+
+store_controller = StoreController()
---
+++ receipt/models.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, String, JSON, Integer
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from db_session import Base
+from primary_model import PrimaryModel
+from store.models import Store
+from user.models import Person
+
+
+class Receipt(PrimaryModel, Base):
+    __tablename__ = 'receipts'
+
+    title = Column(String, nullable=False)
+    payee_id = Column(UUID, ForeignKey=Store.id, nullable=False)
+    payer_id = Column(UUID, ForeignKey=Person.id, nullable=True)
+    payer_name = Column(String)
+    body = Column(JSON, nullable=False)
+    total_payment = Column(Integer, unique=True, nullable=True)
+    details = Column(JSON)
+    status = Column(String, default='Waiting to Pay', nullable=False)
+
+    payee = relationship(Store, primaryjoin=payee_id == Store.id, lazy=True)
+    payer = relationship(Store, primaryjoin=payer_id == Person.id, lazy=True)
+
+    def __init__(self, username):
+        super(Receipt, self).__init__(username)
+
+    def to_dict_(self):
+        result = {
+            'creator': self.creator,
+            'creation_date': self.creation_date,
+            'id': self.id,
+            'version': self.version,
+            'modification_date': self.modification_date,
+            'modifier': self.modifier,
+            'tags': self.tags,
+            'title':self.title,
+            'payee':self.payee.to_dict(),
+            'payer':self.payer.to_dict(),
+            'payee_id': self.payee_id,
+            'payer_name':self.payer_name,
+            'payer_id': self.payer_id,
+            'status':self.status,
+            'body':self.body,
+            'total_payment':self.total_payment,
+            'details':self.details
+        }
+        return result
